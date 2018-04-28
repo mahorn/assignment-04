bde98d39534fd7378230d1a4e27792f35392ef54
mahorn <mmend3@uky.edu>
2018-04-28 17:58:24 -0400

    initial comments to processData.js file

--- build-scripts/processData.js
+++ build-scripts/processData.js
@@ -1,12 +1,15 @@
+// require our edit js modules
 const bindData = require('./bindData.js'),
       colorScheme = require('./colorScheme.js'),
       csvConversion = require('./csv2geojson.js'),
       fs = require('fs');
 
+// create a directory in data path 
 fs.mkdir('data', function (err) {
   if (err) throw err
 });
 
+// constants bonded to newly created files 
 bindData.processBindFiles();
 colorScheme.extractColors();
 csvConversion.convertCsv();
